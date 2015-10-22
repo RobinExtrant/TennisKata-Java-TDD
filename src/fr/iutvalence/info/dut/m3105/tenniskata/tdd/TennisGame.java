1fdffc6e1d23bb26ec5a31d92f9089776723d56c
extrant.robin <robin.extrant@iut-valence.fr>
2015-10-22 10:11:23 +0200

    Do the test for fifteen-all success

--- src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
+++ src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
@@ -2,10 +2,21 @@ package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 
 public class TennisGame
 {
+	private String score;
 	
+	public TennisGame()
+	{
+		this.score="love-all";
+	}
+		
 	public String getScore()
 	{
-		return "love-all";
+		return this.score;
+	}
+
+	public void serverScores()
+	{
+		this.score="fifteen-love";
 	}
 
 }
