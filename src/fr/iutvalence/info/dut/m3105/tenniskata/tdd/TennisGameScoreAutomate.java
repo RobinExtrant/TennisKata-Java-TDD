36f6bbf2cb3c255edff0fd901d1ff2947f52dbd6
extrant.robin <robin.extrant@iut-valence.fr>
2015-10-22 10:51:58 +0200

    refactoring for TennisGameScoreAutomate and method associated but not implemented yet

---
+++ src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameScoreAutomate.java
@@ -0,0 +1,10 @@
+package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
+
+public class TennisGameScoreAutomate {
+
+	public void updateScore(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
