95bc165821d22bd038e3126ba5bed96e41088be2
extrant.robin <robin.extrant@iut-valence.fr>
2015-10-22 10:22:43 +0200

    refactor method receiverScores in TennisGame and extract constant LOVE_FIFTEEN

--- src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
+++ src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
@@ -4,6 +4,7 @@ public class TennisGame
 {
 	public static final String FIFTEEN_LOVE = "fifteen-love";
 	public static final String LOVE_ALL = "love-all";
+	public static final String LOVE_FIFTEEN = "fifteen-love";
 	private String score;
 	
 	public TennisGame()
@@ -19,6 +20,11 @@ public class TennisGame
 	public void serverScores()
 	{
 		this.score=FIFTEEN_LOVE;
+	}
+
+	public void receiverScores()
+	{
+		this.score=LOVE_FIFTEEN;	
 	}
 
 }
